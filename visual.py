e6a489be7d5f64103ad100f0350beaa4ab63d2c7
maddiemooney <m.mooney098@gmail.com>
2018-01-31 10:25:25 -0500

    commenting for my own sanity

--- visual.py
+++ visual.py
@@ -3,31 +3,38 @@ from OpenGL.GLU import *
 from OpenGL.GL import *
 import sys
 
+
+
 name = "Vizualizer Test"
 
 def init():
     glutInit(sys.argv)
+
+    #initializes double buffered window, RGBA mode, depth buffer
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
     glutInitWindowSize(400, 400)
     glutCreateWindow(name)
 
-    # background color
+    # background color (red, green, blue, alpha)
     glClearColor(0., 0., 0., 1.)
 
     # flat or smooth
     glShadeModel(GL_SMOOTH)
 
+    #some graphics stuff about rendering faces
     glEnable(GL_CULL_FACE)
     glEnable(GL_DEPTH_TEST)
 
+    #projection matrix replaced with identity matrix
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
 
 
     glEnable(GL_LIGHTING)
-    lightZeroPosition = [10.,4.,10.,100.]
+    lightZeroPosition = [0.,0.,1.,0.] #aligned with z axis
     lightZeroColor = [1,1,1,1.0] #green tinged
 
+    #(light, source, param)
     glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
     glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
     glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
@@ -36,16 +43,19 @@ def init():
 
 
 def draw_ball():
-    glutSolidSphere(2, 20, 20)
+    #(radius,slices,stacks)
+    glutSolidSphere(2, 10, 10)
 
 
 def display():
-    global sphereLocations
+    #clear
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
     for i in range(-100,100,5):
         glPushMatrix()
-        glTranslatef(i,0,1)
-        color = [1.0,0.,0.,1.]
+        glTranslatef(i,0,0) #translation vector
+        color = [1.0,0.,0.,1.] #red atm
+        #(face, pname, param)
         glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
         draw_ball()
         glPopMatrix()
@@ -59,7 +69,6 @@ def main():
     init()
 
     glutDisplayFunc(display)
-    #glMatrixMode(GL_PROJECTION)
 
     #distance
     gluPerspective(100.,1.,1.,400.)
@@ -69,6 +78,5 @@ def main():
               0,1,0)
     glutMainLoop()
 
-    #return
 
 if __name__ == '__main__': main()
